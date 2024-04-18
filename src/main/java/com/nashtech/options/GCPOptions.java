0064b32167666c49bbde9e748cb07dc9fcb06b93
ankit-mogha <ankit.mogha@nashtechglobal.com>
2024-04-18 16:09:55 +0530

    Updated code to read from pub/sub

--- src/main/java/com/nashtech/options/GCPOptions.java
+++ src/main/java/com/nashtech/options/GCPOptions.java
@@ -1,8 +1,5 @@
 package com.nashtech.options;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.cloud.storage.Storage;
-import com.nashtech.service.GcsClientFactory;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
@@ -16,29 +13,38 @@ public interface GCPOptions extends PipelineOptions {
 
     void setGcpProject(String gcpProject);
 
-    @Description("GCS Bucket Name")
-    @Default.String("gs://code_combat_resumes/resumes-data")
-    String getGcsBucketName();
-
-    void setGcsBucketName(String gcsBucketName);
-
-    @Description("GCS File Name")
-    @Default.String("resumes-data/person-resume-data")
-    String getGcsFileName();
-
-    void setGcsFileName(String gcsFileName);
-
-    @JsonIgnore
-    @Description("GCS Client")
-    @Default.InstanceFactory(GcsClientFactory.class)
-    Storage getGcsClient();
-
-    void setGcsClient(Storage gcsClient);
-
-    @Description("The Cloud Pub/Sub topic to read from.")
+    @Description("The Cloud Pub/Sub topic to read from")
     @Validation.Required
-    @Default.String("projects/nashtech-ai-dev-389315/topics/resume-storage-location")
-    String getTopic();
+    @Default.String("projects/nashtech-ai-dev-389315/topics/resume-input-topic")
+    String getInputTopic();
 
-    void setTopic(String value);
+    void setInputTopic(String inputTopic);
+
+    @Description("The cloud Pub/Sub subscription to read from")
+    @Validation.Required
+    @Default.String("projects/nashtech-ai-dev-389315/subscriptions/resume-input-topic-sub")
+    String getInputSubscription();
+
+    void setInputSubscription(String inputSubscription);
+
+    @Description("The Cloud Pub/Sub topic to write")
+    @Validation.Required
+    @Default.String("projects/nashtech-ai-dev-389315/topics/resume-output-topic")
+    String getOutputTopic();
+
+    void setOutputTopic(String outputTopic);
+
+    @Description("The cloud Pub/Sub subscription to write")
+    @Validation.Required
+    @Default.String("projects/nashtech-ai-dev-389315/subscriptions/resume-output-topic-sub")
+    String getOutputSubscription();
+
+    void setOutputSubscription(String outputSubscription);
+
+    @Description("Whether to use topic or subscription")
+    @Validation.Required
+    @Default.Boolean(true)
+    Boolean getUseSubscription();
+
+    void setUseSubscription(Boolean useSubscription);
 }
